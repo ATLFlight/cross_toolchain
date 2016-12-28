3d3dabbe355cc5d4a63b86fc873635fce97569e3
Mark Charlebois <charlebm@gmail.com>
2016-12-27 16:54:53 -0800

    Changed name of created file to minimalSDK.tgz
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- createMinimalSDK.sh
+++ createMinimalSDK.sh
@@ -16,7 +16,7 @@ export HEXAGON_TOOLS_ROOT=`pwd`/tmpSDK/Qualcomm/HEXAGON_Tools/7.2.12/Tools
 # Unpack and trim the qrlSDK
 ./qrlinux_sysroot.sh -trim
 
-# Create a tgz of the trimmed install for use in a docker image
+# Create a tgz of the minimal install for use in a docker image
 cd tmpSDK
-tar czf ../trimmedSDK.tgz Qualcomm
+tar czf ../minimalSDK.tgz Qualcomm
 
